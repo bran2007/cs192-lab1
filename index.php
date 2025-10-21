7c0e3e8967a5451980467767a41fb4fdf8f0c4b4
Brandon Chang <62526752+bran2007@users.noreply.github.com>
2025-10-20 21:00:06 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head><meta charset="UTF-8"></head>
+<body>
+<?php
+/*
+ * Who: Brandon Chang
+ * What: CMP SCI cs192 Lab Web Page Project
+ * When: 10/20/25
+ * What: Lab Web Page #1
+*/
+
+// Lab 1 Assignment
+echo "<h2>Lab 1 Assignment</h2>";
+echo "<p>My name is Brandon Chang</p>";
+
+?>
+</body>
+</html>
+<!-- Brandon Chang, CMP SCI cs192 Lab Web Page Project, Lab Web Page #1  -->

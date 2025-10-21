0137984041aca62acfe0da31b55507376e595483
Brandon Chang <62526752+bran2007@users.noreply.github.com>
2025-10-20 21:26:21 -0700

    Update index.php for Lab Web Page #2 inclusion

--- index.php
+++ index.php
@@ -7,14 +7,25 @@
  * Who: Brandon Chang
  * What: CMP SCI cs192 Lab Web Page Project
  * When: 10/20/25
- * What: Lab Web Page #1
+ * What: Lab Web Page #1 and #2
 */
 
-// Lab 1 Assignment
-echo "<h2>Lab 1 Assignment</h2>";
-echo "<p>My name is Brandon Chang</p>";
+// -------- LAB 2 ---------
+$coname623 = "House_R_Us";
+$coaddr812 = "123 Main St";
+$co_city511 = "Miami FL 33101";
+
+print "<h2>Lab 2 Assignment</h2>";
+print "<h1>$coname623</h1>";
+print "<p>$coaddr812</p>";
+print "<b>$co_city511</b>";
+print "<hr>";
+
+// -------- LAB 1 ---------
+print "<h2>Lab 1 Assignment</h2>";
+print "<p>My name is Brandon Chang</p>";
 
 ?>
 </body>
 </html>
-<!-- Brandon Chang, CMP SCI cs192 Lab Web Page Project, Lab Web Page #1  -->
+<!-- Brandon Chang, CMP SCI cs192 Lab Web Page Project, Lab Web Page #1 and #2 -->

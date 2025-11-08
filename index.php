afb95a7c8e1465322b5a3854f299c90b5b916d0e
Brandon Chang <62526752+bran2007@users.noreply.github.com>
2025-11-07 17:57:20 -0800

    Refactor index.php to use classes and functions

--- index.php
+++ index.php
@@ -10,115 +10,6 @@
  * What: PHP Program for Lab Web Page #1
 */
 
-// ----------------------   Lab 1   -----------------------
-print "<h2>Lab 1 Assignment</h2>";
-
-function display_name_101() {
-    print "<h3>Brandon Chang Vacations</h3>";
-}
-
-display_name_101();
-
-print "<hr>";
-
-// ----------------------   Lab 2   -----------------------
-print "<h2>Lab 2 Assignment</h2>";
-
-function display_address_202() {
-    print "123 Main St<br>Miami, FL 33101";
-}
-
-display_address_202();
-
-print "<hr>";
-
-// ----------------------   Lab 3   -----------------------
-print "<h2>Lab 3 Assignment</h2>";
-
-$company_name = "Brandon Chang Vacations";
-$company_address = "123 Main St";
-$company_city = "Miami, FL 33101";
-
-function display_name_303($name) {
-    print "<h3>$name</h3>";
-}
-
-function display_address_303($address, $city) {
-    print "$address<br>$city";
-}
-
-display_name_303($company_name);
-display_address_303($company_address, $company_city);
-
-print "<hr>";
-
-// ----------------------   Lab 4   -----------------------
-print "<h2>Lab 4 Assignment</h2>";
-
-function display_name_404($name, $color) {
-    print "<table style='background-color:$color; width:100%'><tr><td align='center'>";
-    print "<h1>$name</h1>";
-    print "</td></tr></table>";
-}
-
-function display_address_404($color, $address, $city) {
-    print "<table style='background-color:$color; width:100%'><tr><td align='center'>";
-    print "$address<br>$city";
-    print "</td></tr></table>";
-}
-
-display_name_404($company_name, "lightblue");
-display_address_404("lightgray", $company_address, $company_city);
-
-print "<hr>";
-
-// ----------------------   Lab 5   -----------------------
-print "<h2>Lab 5 Assignment</h2>";
-
-function display_name_505() {
-    global $company_name;
-    print "<table style='background-color:pink; width:100%'><tr><td align='center'>";
-    print "<h1>$company_name</h1>";
-    print "</td></tr></table>";
-}
-
-function display_address_505() {
-    global $company_address, $company_city;
-    print "<table style='background-color:yellow; width:100%'><tr><td align='center'>";
-    print "$company_address<br>$company_city";
-    print "</td></tr></table>";
-}
-
-display_name_505();
-print "<br><br>";
-display_address_505();
-
-print "<hr>";
-
-// ----------------------   Lab 6   -----------------------
-print "<h2>Lab 6 Assignment</h2>";
-
-function getHeader935($company_name, $color) {
-    $data  = "<table style='background-color:$color;width:100%'><tr><td>";
-    $data .= "<h1 style='text-align:center'>$company_name</h1>";
-    $data .= "</td></tr></table>";
-    return $data;
-}
-
-function getFooter732($color) {
-    global $company_name, $company_address, $company_city;
-    $data  = "<table style='background-color:$color;width:100%'><tr><td>";
-    $data .= "<p style='text-align:center'>$company_name<br>$company_address<br>$company_city</p>";
-    $data .= "</td></tr></table>";
-    return $data;
-}
-
-print getHeader935($company_name, "lightgreen");
-print "<br><br>";
-print getFooter732("lightyellow");
-
-print "<hr>";
-
 // ----------------------   Lab 7   -----------------------
 print "<h2>Lab 7 Assignment</h2>";
 
@@ -152,9 +43,115 @@ print $object593->getFooter732();
 
 print "<hr>";
 
-// ---------   WARNING: CODE SHIFT   ---------
-// ---------   Above this line is OOP   -------
-// ---------   Below this line is procedural  --
+// ----------------------   Lab 6   -----------------------
+print "<h2>Lab 6 Assignment</h2>";
+
+function getHeader935($company_name, $color) {
+    $data  = "<table style='background-color:$color;width:100%'><tr><td>";
+    $data .= "<h1 style='text-align:center'>$company_name</h1>";
+    $data .= "</td></tr></table>";
+    return $data;
+}
+
+function getFooter732($color) {
+    global $company_name, $company_address, $company_city;
+    $data  = "<table style='background-color:$color;width:100%'><tr><td>";
+    $data .= "<p style='text-align:center'>$company_name<br>$company_address<br>$company_city</p>";
+    $data .= "</td></tr></table>";
+    return $data;
+}
+
+$company_name = "Brandon Chang Vacations";
+$company_address = "123 Main St";
+$company_city = "Miami, FL 33101";
+
+print getHeader935($company_name, "lightgreen");
+print "<br><br>";
+print getFooter732("lightyellow");
+
+print "<hr>";
+
+// ----------------------   Lab 5   -----------------------
+print "<h2>Lab 5 Assignment</h2>";
+
+function display_name_505() {
+    global $company_name;
+    print "<table style='background-color:pink; width:100%'><tr><td align='center'>";
+    print "<h1>$company_name</h1>";
+    print "</td></tr></table>";
+}
+
+function display_address_505() {
+    global $company_address, $company_city;
+    print "<table style='background-color:yellow; width:100%'><tr><td align='center'>";
+    print "$company_address<br>$company_city";
+    print "</td></tr></table>";
+}
+
+display_name_505();
+print "<br><br>";
+display_address_505();
+
+print "<hr>";
+
+// ----------------------   Lab 4   -----------------------
+print "<h2>Lab 4 Assignment</h2>";
+
+function display_name_404($name, $color) {
+    print "<table style='background-color:$color; width:100%'><tr><td align='center'>";
+    print "<h1>$name</h1>";
+    print "</td></tr></table>";
+}
+
+function display_address_404($color, $address, $city) {
+    print "<table style='background-color:$color; width:100%'><tr><td align='center'>";
+    print "$address<br>$city";
+    print "</td></tr></table>";
+}
+
+display_name_404($company_name, "lightblue");
+display_address_404("lightgray", $company_address, $company_city);
+
+print "<hr>";
+
+// ----------------------   Lab 3   -----------------------
+print "<h2>Lab 3 Assignment</h2>";
+
+function display_name_303($name) {
+    print "<h3>$name</h3>";
+}
+
+function display_address_303($address, $city) {
+    print "$address<br>$city";
+}
+
+display_name_303($company_name);
+display_address_303($company_address, $company_city);
+
+print "<hr>";
+
+// ----------------------   Lab 2   -----------------------
+print "<h2>Lab 2 Assignment</h2>";
+
+function display_address_202() {
+    print "123 Main St<br>Miami, FL 33101";
+}
+
+display_address_202();
+
+print "<hr>";
+
+// ----------------------   Lab 1   -----------------------
+print "<h2>Lab 1 Assignment</h2>";
+
+function display_name_101() {
+    print "<h3>Brandon Chang Vacations</h3>";
+}
+
+display_name_101();
+
+print "<hr>";
+
 ?>
 </body>
 </html>
